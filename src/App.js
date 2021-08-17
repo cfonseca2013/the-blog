efc06565d1bbd36c1e3076c9792554336cd2ad55
Chris Fonseca <fonseca.j.chris@gmail.com>
2021-08-16 23:03:38 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route, Switch
+} from 'react-router-dom';
+import HomePage  from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticleListPage from './pages/ArticleListPage';
+import NavBar from './NavBar';
+import NotFoundPage from './pages/NotFoundPage';
 import './App.css';
 
-function App() {
+
+
+class App extends Component {
+  render(){
   return (
+    <Router >
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <div id="page-body">
+        <Switch>
+        <Route path="/" component= {HomePage} exact/>
+        <Route path="/about" component= {AboutPage} exact/>
+        <Route path="/articles-list" component= {ArticleListPage} exact/>
+        <Route path="/article/:name" component= {ArticlePage} exact/>
+        <Route component= {NotFoundPage} exact />
+        </Switch>
+        </div>
     </div>
+    </Router>
   );
+}
 }
 
 export default App;
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+    <h1>About Me</h1>
+    <p>This is the cool blog.</p>
+    </>
+)
+
+
+
+
+
+export default AboutPage;
+
